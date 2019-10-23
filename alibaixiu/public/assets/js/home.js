2dca6d609c6b42ab4f2391c42013eca3c251c653
vscolor <798209437@qq.com>
2019-10-23 11:06:18 +0800

    实现首页轮播图数据展示

---
+++ alibaixiu/public/assets/js/home.js
@@ -0,0 +1,31 @@
+// 向服务器端发送请求 索要轮播图数据
+$.ajax({
+    type: 'get',
+    url: '/slides',
+    success: function(response) {
+        // console.log(response);
+        var html = template('slidesTpl', { data: response });
+        // console.log(html)
+        $('#slidesBox').html(html)
+            //
+        var swiper = Swipe(document.querySelector('.swipe'), {
+            auto: 3000,
+            transitionEnd: function(index) {
+                // index++;
+
+                $('.cursor span').eq(index).addClass('active').siblings('.active').removeClass('active');
+            }
+        });
+
+        // 上/下一张
+        $('.swipe .arrow').on('click', function() {
+            var _this = $(this);
+
+            if (_this.is('.prev')) {
+                swiper.prev();
+            } else if (_this.is('.next')) {
+                swiper.next();
+            }
+        })
+    }
+})
